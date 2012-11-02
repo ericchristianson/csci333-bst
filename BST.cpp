62e88fcf05a6d8bdeeec75f175110d37423e9157
eric <echristi@unca.edu>
2012-11-02 15:20:47 -0400

    rudamentary remove

--- BST.cpp
+++ BST.cpp
@@ -36,8 +36,91 @@ void BST<T>::insert(T v) {
 
 template <typename T>
 void BST<T>::remove(T v) {
-  Node<T>* temp = new Node<T>(v);
-  root = temp;
+  Node<T>* temp = root;
+  Node<T>* parent;
+
+  //Find the Node
+  while(temp != 0 && temp->getValue() != v){
+    parent = temp;
+    if(v > temp->getRightChild()){
+      temp = temp->getRightChild();
+    }
+    else{
+      temp = temp->getLeftChild();
+    }
+  }
+
+//Node has two children:
+  if(temp->getLeftChild() != 0 && temp->getRightChild() != 0){
+    //Go right once, check for kids
+    Node<T>* curr = temp->getRightChild();
+    if(curr->getLeftChild == 0 && curr->getRightChild == 0){
+      temp = curr;
+      delete temp;
+      curr->getRightChild = NULL;
+    }
+    else{//if right child haz children, go all the way left.
+      if(temp->getRightChild()->getLeftChild != 0){
+        Node<T>* leftChild;
+        Node<T>* leftChildParent;
+        leftChildParent = temp->getRightChild();
+        leftChild = temp->getRightChild()->getLeftChild();
+        while(leftChildParent->getLeftChild() != 0){
+          leftChildParent = leftChild;
+          leftChild = leftChild->getLeftChild();
+        }
+//?????
+        temp->getValue() = leftChild->getValue();
+        delete leftChild;
+        leftChildParent->getLeftChild() = NULL;
+      }
+      else{
+        Node<T>* curr;
+        curr = temp->getRightChild();
+        temp->getValue() = curr->getValue();
+        temp->getRightChild() = curr->getRightChild();
+        delete temp;
+      }
+    }
+    return;    
+  }
+
+//Node has 1 child:
+  if(temp->getLeftChild()==0 && temp->getRightChild() != 0 || temp->getLeftChild() != 0 && temp->getRightChild()==0){
+    //No left child
+    if(temp->getLeftChild()==0 && temp->getRightChild()!=0){
+      if(parent->getLeftChild() == temp){
+        parent->getLeftChild() = temp->getRightChild();
+        delete temp;
+      }
+      else{
+        parent->getRightChild() = temp->getRightChild();
+        delete temp;
+      }
+    }
+  
+    else{ //No right child
+      if(parent->getLeftChild() == temp){
+        parent->getLeftChild() = temp->getLeftChild();
+        delete temp;
+      }
+      else{
+        parent->getRightChild() = temp->getLeftChild();
+        delete temp;
+      }
+    }
+    return;
+  }
+
+//Node is a leaf
+  if(parent->getLeftChild() == temp){
+    parent->getLeftChild() = NULL;
+  }
+  else{
+    parent->getRightChild() = NULL;
+  }
+  delete temp;
+  return;
 }
 
 template <typename T>
